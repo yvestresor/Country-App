74ac59f099737c12b95ae8e2434b1f04d98aedf9
Treasure <133379929+yvestresor@users.noreply.github.com>
2023-08-17 16:32:08 +0300

    Create Country App

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# Country-App
+## Countries Information App
+
+This project challenges your proficiency in HTML, CSS, and JavaScript, and offers an opportunity to integrate with the REST Countries API to showcase country data in alignment with the provided design.
+
+## Challenge Objectives
+
+Your task is to create an interactive web application that meets the following requirements:
+
+1. Display a list of countries sourced from the API on the main page.
+2. Implement a search feature allowing users to find specific countries using an input field.
+3. Provide the functionality to filter countries by region.
+4. Enable users to click on a country to view detailed information on a separate page.
+5. Allow users to navigate to the bordering countries from the detail page.
+6. (Optional) Incorporate a toggle feature for switching between light and dark color schemes.
+
+## Implementation
+
+You are free to utilize any front-end JavaScript framework or library, such as React or Vue, to accomplish this challenge. The choice of packages for tasks like making HTTP requests or styling your project is entirely up to you.
+
+## Getting Started
+
+Follow these steps to get started:
+
+1. Clone this repository to your local machine.
+2. Set up your preferred development environment.
+3. Explore the provided design files to understand the expected layout and styling.
+4. Integrate with the REST Countries API to fetch country data.
+5. Implement the required functionality based on the challenge objectives.
+6. Test your application thoroughly to ensure all features are working as intended.
+
+## Resources
+
+* REST Countries API Documentation - Refer to the API documentation for details on retrieving country data.
+
+## Submission
+
+Once you've completed the challenge, submit your solution by creating a pull request to this repository. Make sure to include a brief description of your implementation choices and any challenges you encountered.
+
+Good luck and happy coding!
+
+
+
+
+
---
+++ script.js
@@ -0,0 +1,88 @@
+// console.log('baron'); //testing your js file
+
+//globall variables
+const BASE_API = "https://restcountries.com/v3.1/all";
+const countryDetailsContent = document.querySelector('.country-details-content');
+let allCountries = [];
+
+//api call function
+const endPoints = async () =>{
+    try{
+        const data = await fetch(`${BASE_API}`);
+        const response = await data.json();
+        allCountries = response;
+    }catch (error){
+        console.log(error);
+        return error;
+    }
+}
+
+//search by country
+const searchBtn = document.getElementById('searchBtn');
+const selectedCountryName = document.querySelector('.input');
+
+selectedCountryName.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter'){
+        searchCountry();
+    }
+})
+
+//creating the div to show details about a country
+const showCountry = async (parsedCountryData) =>{
+    var currencies = '';
+    const currenciesObject = parsedCountryData?.currencies || {};
+    for (const currency in currenciesObject) {
+        if (currenciesObject.hasOwnProperty(currency)) {
+            currencies = currencies + parsedCountryData.currencies[currency]?.name + '(' + parsedCountryData.currencies[currency]?.symbol + '), ';
+        }
+      }
+    currencies = currencies.substring(0, currencies.length - 2);
+    var languages = '';
+    const languagesObject = parsedCountryData?.languages || {};
+    for (const language in languagesObject) {
+        if (languagesObject.hasOwnProperty(language)) {
+            languages = languages + parsedCountryData.languages[language] + ', ';
+        }
+      }
+    languages = languages.substring(0, languages.length - 2);
+    countryDetailsContent.innerHTML = `
+        <div class="countryflag">
+            <img src="${parsedCountryData.flags.png}" alt="">
+        </div>
+        <div class="countryinfo">
+            <div>
+                <h3>${parsedCountryData.name.common}</h3>
+                <p><strong>Population: </strong>${parsedCountryData.population}</p>
+                <p><strong>Region: </strong>${parsedCountryData.region}</p>
+                <p><strong>Sub Region: </strong>${parsedCountryData.subregion}</p>
+                <p><strong>Capital: </strong>${parsedCountryData.capital}</p>
+                <p><strong>Area: </strong>${parsedCountryData.area} km2</p>
+                <!-- Add more details as needed -->
+            </div>
+            <div>
+                <p><strong>Currencies: </strong>${currencies}</p>
+                <p><strong>Languages: </strong>${languages}</p>
+                <p><strong>View On Map: </strong><a href='${parsedCountryData.maps.googleMaps}' target='_blank'>${parsedCountryData.maps.googleMaps}</a></p>
+                <p><strong>Timezones: </strong>${parsedCountryData.timezones}</p>
+                <p><strong>Border Countries: </strong>${parsedCountryData.borders}</p>
+            </div>
+        </div>`;
+}
+
+
+function searchCountry() {
+    if (selectedCountryName.value.trim() !== '') {
+        const selectedCountry = allCountries.find(countrySearch => countrySearch.name.common.toLowerCase().includes(selectedCountryName.value.trim().toLowerCase()));
+        // const selectedCountry = allCountries.find(countrySearch => countrySearch.name.common.toLowerCase() === selectedCountryName.value.trim().toLowerCase());
+        if (selectedCountry) {
+            countryDetailsContent.innerHTML = '';
+            showCountry(selectedCountry);
+        } else {
+            countryDetailsContent.innerHTML = '<p>No matching country found!</p>';
+        }
+    }
+}
+
+
+endPoints();
+ 
